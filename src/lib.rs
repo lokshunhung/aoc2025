da5db0c03e10e7d08793c6d7b351a0e7bce5489c
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2025-12-16 12:05:33 +0800

    add template for new modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+#![allow(dead_code)]
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use std::io;
+    use std::io::BufRead;
+    use std::path;
+
+    use super::*;
+
+    #[test]
+    fn test() {
+        let pb = path::Path::new(file!()).parent().unwrap().join("input.txt");
+        let f = fs::File::open(&pb).unwrap();
+        let br = io::BufReader::new(f).lines();
+
+        for line in br.map_while(Result::ok) {
+            //
+        }
+    }
+}
+
 pub mod d1p1;
 pub mod d1p2;
 pub mod d2p1;
