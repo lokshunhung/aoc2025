e214bf0f6f398cb9de608f817741c353676d24c6
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2025-12-16 12:41:42 +0800

    impl d3p1

---
+++ src/d3p1/mod.rs
@@ -0,0 +1,74 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Bank(Vec<u8>);
+
+impl Bank {
+    fn get_largest(&self) -> u64 {
+        let mut m1 = self.0.len() - 2;
+        let mut m2 = self.0.len() - 1;
+        for i in (0..self.0.len() - 2).rev() {
+            let n = self.0[i];
+            if n >= self.0[m1] {
+                if self.0[m1] >= self.0[m2] {
+                    m2 = m1;
+                }
+                m1 = i;
+            }
+        }
+        self.0[m1] as u64 * 10 + self.0[m2] as u64
+    }
+}
+
+impl TryFrom<&str> for Bank {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let vec = value
+            .chars()
+            .map(|c| c.to_digit(10).map(|d| d as u8))
+            .collect::<Option<Vec<_>>>()
+            .ok_or_else(|| format!("cannot parse {}", value))?;
+        Ok(Self(vec))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+    use std::io;
+    use std::io::BufRead;
+    use std::path;
+
+    use super::*;
+
+    #[test]
+    fn test() {
+        let pb = path::Path::new(file!()).parent().unwrap().join("input.txt");
+        let f = fs::File::open(&pb).unwrap();
+        let br = io::BufReader::new(f).lines();
+
+        let mut answer = 0;
+
+        for line in br.map_while(Result::ok) {
+            let bank = Bank::try_from(line.as_str()).unwrap();
+            answer += bank.get_largest();
+        }
+
+        println!("{}", answer);
+    }
+
+    #[test]
+    fn sanity() {
+        assert_eq!(Bank(vec![1, 2, 3, 4]).get_largest(), 34);
+        assert_eq!(Bank(vec![9, 1, 1, 1, 2]).get_largest(), 92);
+    }
+
+    #[test]
+    fn from_example() {
+        assert_eq!(Bank::try_from("987654321111111").unwrap().get_largest(), 98);
+        assert_eq!(Bank::try_from("811111111111119").unwrap().get_largest(), 89);
+        assert_eq!(Bank::try_from("234234234234278").unwrap().get_largest(), 78);
+        assert_eq!(Bank::try_from("818181911112111").unwrap().get_largest(), 92);
+    }
+}
