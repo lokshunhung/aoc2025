c75151ba73d4baeb5336b44d12e2f4335e11f242
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2025-12-17 11:16:30 +0800

    refactor d4

--- src/d4p1/mod.rs
+++ src/d4p1/mod.rs
@@ -1,64 +1,74 @@
 #![allow(dead_code)]
 
-use std::fs;
-use std::io;
+use std::fs::File;
 use std::io::BufRead;
-use std::path;
+use std::io::BufReader;
+use std::io::Lines;
+use std::path::Path;
 
-fn parse(path: impl AsRef<std::path::Path>) -> Vec<Vec<char>> {
-    let pb = path::Path::new(file!()).parent().unwrap().join(path);
-    let f = fs::File::open(&pb).unwrap();
-    let br = io::BufReader::new(f).lines();
-
-    let mut grid = Vec::<Vec<char>>::new();
-
-    for line in br.map_while(Result::ok) {
-        grid.push(line.chars().collect());
+struct Fixture(&'static str);
+impl Fixture {
+    fn reader(&self) -> BufReader<File> {
+        let path = Path::new(file!()).parent().unwrap().join(self.0);
+        let file = File::open(&path).unwrap();
+        BufReader::new(file)
     }
-
-    grid
 }
 
-fn solve(grid: &[Vec<char>]) -> i32 {
-    let mut cnt = 0;
+struct Grid(Vec<Vec<char>>);
+impl From<Lines<BufReader<File>>> for Grid {
+    fn from(lines: Lines<BufReader<File>>) -> Self {
+        let mut grid = Vec::new();
+        for line in lines.map_while(Result::ok) {
+            grid.push(line.chars().collect());
+        }
+        Self(grid)
+    }
+}
+impl Grid {
+    fn solve(&self) -> i32 {
+        let grid = &self.0;
 
-    for y in 0..grid.len() as isize {
-        for x in 0..grid[y as usize].len() as isize {
-            let cell = grid[y as usize][x as usize];
-            if cell != '@' {
-                print!("{}", cell);
-                continue;
-            }
+        let mut cnt = 0;
 
-            let mut adj = 0;
-            for xx in (x - 1)..=(x + 1) {
-                for yy in (y - 1)..=(y + 1) {
-                    if xx == x && yy == y {
-                        continue;
-                    }
-                    if !(0 <= xx && xx < grid[y as usize].len() as isize) {
-                        continue;
-                    }
-                    if !(0 <= yy && yy < grid.len() as isize) {
-                        continue;
-                    }
-                    if grid[yy as usize][xx as usize] == '@' {
-                        adj += 1;
+        for y in 0..grid.len() as isize {
+            for x in 0..grid[y as usize].len() as isize {
+                let cell = grid[y as usize][x as usize];
+                if cell != '@' {
+                    print!("{}", cell);
+                    continue;
+                }
+
+                let mut adj = 0;
+                for xx in (x - 1)..=(x + 1) {
+                    for yy in (y - 1)..=(y + 1) {
+                        if xx == x && yy == y {
+                            continue;
+                        }
+                        if !(0 <= xx && xx < grid[y as usize].len() as isize) {
+                            continue;
+                        }
+                        if !(0 <= yy && yy < grid.len() as isize) {
+                            continue;
+                        }
+                        if grid[yy as usize][xx as usize] == '@' {
+                            adj += 1;
+                        }
                     }
                 }
-            }
 
-            if adj < 4 {
-                cnt += 1;
-                print!("x");
-            } else {
-                print!("{}", cell);
+                if adj < 4 {
+                    cnt += 1;
+                    print!("x");
+                } else {
+                    print!("{}", cell);
+                }
             }
+            println!();
         }
-        println!();
-    }
 
-    cnt
+        cnt
+    }
 }
 
 #[cfg(test)]
@@ -67,16 +77,23 @@ mod test {
 
     #[test]
     fn d4p1() {
-        let grid = parse("input.txt");
-        let cnt = solve(&grid);
-        println!("{}", cnt);
+        let fixture = Fixture("input.txt");
+        let lines = fixture.reader().lines();
+        let grid = Grid::from(lines);
+        let answer = grid.solve();
+
+        println!("{}", answer);
+
+        assert_eq!(answer, 1395);
     }
 
     #[test]
     fn sample() {
-        let grid = parse("sample.txt");
-        let cnt = solve(&grid);
+        let fixture = Fixture("sample.txt");
+        let lines = fixture.reader().lines();
+        let grid = Grid::from(lines);
+        let answer = grid.solve();
 
-        assert_eq!(cnt, 13);
+        assert_eq!(answer, 13);
     }
 }
--- src/d4p2/mod.rs
+++ src/d4p2/mod.rs
@@ -1,80 +1,92 @@
 #![allow(dead_code, unused)]
 
-use std::fs;
-use std::io;
+use std::fs::File;
 use std::io::BufRead;
-use std::path;
+use std::io::BufReader;
+use std::io::Lines;
+use std::path::Path;
 
-fn parse(path: impl AsRef<std::path::Path>) -> Vec<Vec<char>> {
-    let pb = path::Path::new(file!()).parent().unwrap().join(path);
-    let f = fs::File::open(&pb).unwrap();
-    let br = io::BufReader::new(f).lines();
-
-    let mut grid = Vec::<Vec<char>>::new();
-
-    for line in br.map_while(Result::ok) {
-        grid.push(line.chars().collect());
+struct Fixture(&'static str);
+impl Fixture {
+    fn reader(&self) -> BufReader<File> {
+        let path = Path::new(file!()).parent().unwrap().join(self.0);
+        let file = File::open(&path).unwrap();
+        BufReader::new(file)
     }
-
-    grid
 }
 
-fn solve_once(grid: &mut [Vec<char>], cont: &mut bool) -> i32 {
-    let mut cnt = 0;
+struct Grid(Vec<Vec<char>>);
+impl From<Lines<BufReader<File>>> for Grid {
+    fn from(lines: Lines<BufReader<File>>) -> Self {
+        let mut grid = Vec::new();
+        for line in lines.map_while(Result::ok) {
+            grid.push(line.chars().collect());
+        }
+        Self(grid)
+    }
+}
 
-    for y in 0..grid.len() as isize {
-        for x in 0..grid[y as usize].len() as isize {
-            let cell = grid[y as usize][x as usize];
-            if cell != '@' {
-                print!("{}", cell);
-                continue;
-            }
+impl Grid {
+    fn solve_once(&mut self, cont: &mut bool) -> i32 {
+        let grid = &mut self.0;
 
-            let mut adj = 0;
-            for xx in (x - 1)..=(x + 1) {
-                for yy in (y - 1)..=(y + 1) {
-                    if xx == x && yy == y {
-                        continue;
-                    }
-                    if !(0 <= xx && xx < grid[y as usize].len() as isize) {
-                        continue;
-                    }
-                    if !(0 <= yy && yy < grid.len() as isize) {
-                        continue;
-                    }
-                    if grid[yy as usize][xx as usize] == '@' {
-                        adj += 1;
+        let mut cnt = 0;
+
+        for y in 0..grid.len() as isize {
+            for x in 0..grid[y as usize].len() as isize {
+                let cell = grid[y as usize][x as usize];
+                if cell != '@' {
+                    print!("{}", cell);
+                    continue;
+                }
+
+                let mut adj = 0;
+                for xx in (x - 1)..=(x + 1) {
+                    for yy in (y - 1)..=(y + 1) {
+                        if xx == x && yy == y {
+                            continue;
+                        }
+                        if !(0 <= xx && xx < grid[y as usize].len() as isize) {
+                            continue;
+                        }
+                        if !(0 <= yy && yy < grid.len() as isize) {
+                            continue;
+                        }
+                        if grid[yy as usize][xx as usize] == '@' {
+                            adj += 1;
+                        }
                     }
                 }
-            }
 
-            if adj < 4 {
-                cnt += 1;
-                print!("x");
-                grid[y as usize][x as usize] = 'x';
-                *cont = true;
-            } else {
-                print!("{}", cell);
+                if adj < 4 {
+                    cnt += 1;
+                    print!("x");
+                    grid[y as usize][x as usize] = 'x';
+                    *cont = true;
+                } else {
+                    print!("{}", cell);
+                }
             }
+            println!();
         }
-        println!();
+
+        cnt
     }
 
-    cnt
-}
-
-fn solve(grid: &mut [Vec<char>]) -> i32 {
-    let mut answer = 0;
-    let mut cont: bool;
-    loop {
-        cont = false;
-        answer += solve_once(grid, &mut cont);
-        if !cont {
-            break;
+    fn solve(&mut self) -> i32 {
+        let mut answer = 0;
+        let mut cont: bool;
+        loop {
+            cont = false;
+            answer += self.solve_once(&mut cont);
+            if !cont {
+                break;
+            }
+            println!();
         }
-        println!();
+
+        answer
     }
-    answer
 }
 
 #[cfg(test)]
@@ -88,16 +100,22 @@ mod test {
 
     #[test]
     fn d4p2() {
-        let mut grid = parse("input.txt");
-        let answer = solve(&mut grid);
+        let fixture = Fixture("input.txt");
+        let lines = fixture.reader().lines();
+        let mut grid = Grid::from(lines);
+        let answer = grid.solve();
 
         println!("{}", answer);
+
+        assert_eq!(answer, 8451);
     }
 
     #[test]
     fn sample() {
-        let mut grid = parse("sample.txt");
-        let answer = solve(&mut grid);
+        let fixture = Fixture("sample.txt");
+        let lines = fixture.reader().lines();
+        let mut grid = Grid::from(lines);
+        let answer = grid.solve();
 
         assert_eq!(answer, 43);
     }
